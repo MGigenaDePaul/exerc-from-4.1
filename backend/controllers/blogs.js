d4240bfcca1b95aa6c35464db801af9a4dc42a18
Miqueas <miqueasgigena18@gmail.com>
2025-08-07 13:07:54 -0300

    Exercise 4.13

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -15,7 +15,13 @@ blogsRouter.post('/', async (request, response) => {
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
-
 })
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+}) /*because we have express@5.1.0 we do not need to
+install the express-async-errors library, express 5 does all the error hanlder work under the hood
+*/
 
 module.exports = blogsRouter
--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -1,5 +1,5 @@
 const assert = require('node:assert')
-const { test, after, beforeEach } = require('node:test')
+const { test, after, describe, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -12,71 +12,94 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned in JSON format', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned in JSON format', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
-})
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
 
-test('verifies that the unique identifier property of the blog posts is named id', async () => {
-  const response = await api.get('/api/blogs')
-
-  response.body.forEach(blog => {
-    assert(Object.hasOwn(blog, 'id'))
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
 })
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'NEW BLOG',
-    author: 'Reacon',
-    url: 'https://lslslslsls',
-    likes: 4,
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-
-  const contents = blogsAtEnd.map((blog) => blog.title)
-  assert(contents.includes('NEW BLOG'))
-})
-
-test('if the likes property is missing from the request, it will default to the value 0', async () => {
-  const response = await api.get('/api/blogs')
-
-  response.body.forEach((blog) => {
-    if (!Object.hasOwn(blog, 'likes')) {
-      assert.strictEqual(blog.likes, 0)
+describe('viewing a specific blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'NEW BLOG',
+      author: 'Reacon',
+      url: 'https://lslslslsls',
+      likes: 4,
     }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    const contents = blogsAtEnd.map((blog) => blog.title)
+    assert(contents.includes('NEW BLOG'))
   })
 })
 
-test('if the title or url properties are missing, the backend responds with the status code 400 Bad Request.', async () => {
-  const newBlog = {
-    author: 'no title or url',
-    likes: 4
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+describe('viewing properties of blogs', () => {
+  test('verifies that the unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
 
-  const blogsAtEnd = await helper.blogsInDb()
+    response.body.forEach(blog => {
+      assert(Object.hasOwn(blog, 'id'))
+    })
+  })
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+
+  test('if the likes property is missing from the request, it will default to the value 0', async () => {
+    const response = await api.get('/api/blogs')
+
+    response.body.forEach((blog) => {
+      if (!Object.hasOwn(blog, 'likes')) {
+        assert.strictEqual(blog.likes, 0)
+      }
+    })
+  })
+
+  test('if the title or url properties are missing, the backend responds with the status code 400 Bad Request.', async () => {
+    const newBlog = {
+      author: 'no title or url',
+      likes: 4
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+  })
+})
+
+describe('deletion of a blog', () => {
+  test('succeed with status 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    const contents = blogsAtEnd.map(b => b.title)
+
+    assert(!contents.includes(blogToDelete.title))
+  })
 })
 
 after(async () => {
