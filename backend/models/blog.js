5b21208a29a8703f87798bc3bd19eac16e3f3175
Miqueas <miqueasgigena18@gmail.com>
2025-08-20 17:20:58 -0300

    Refactor API responses for better order and legibility

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -4,13 +4,13 @@ const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: {
-    type: Number,
-    default: 0
-  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
+  },
+  likes: {
+    type: Number,
+    default: 0
   }
 })
 
