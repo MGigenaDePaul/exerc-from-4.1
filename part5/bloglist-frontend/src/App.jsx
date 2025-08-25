35191175009f2392fb4ea8f26a3d94a4d9c1775c
Miqueas <miqueasgigena18@gmail.com>
2025-08-25 12:45:13 -0300

    Exercise 5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import Notification from './components/Notification'
 import loginService from './services/login' 
+import './index.css' 
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -11,6 +13,7 @@ const App = () => {
   const [newTitle, setNewTitle] = useState('')
   const [newAuthor, setNewAuthor] = useState('')
   const [newUrl, setNewUrl] = useState('')
+  const [message, setMessage] = useState(null)
   
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -39,9 +42,12 @@ const App = () => {
       setPassword('')
     } 
     catch {
-      console.log('wrong credentials, failed to log in')
-    }
+      setMessage(`wrong username or password`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
   }
+}
 
   const handleLogOut = async() => {
     try {
@@ -61,6 +67,7 @@ const App = () => {
     return (
       <div>
         <h2>Log in to application</h2>
+        <Notification message={message} />
         <form onSubmit={handleLogin}>
           <div>
             <label>
@@ -95,39 +102,43 @@ const App = () => {
       setNewTitle('')
       setNewAuthor('')
       setNewUrl('')
+      setMessage(`a new blog ${blogObject.title} by ${blogObject.author} added`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     })
   }
-  
 
   return (
     <div> 
-      {!user && loginForm()}
-      {user && (
-        <div>
-          <h2>blogs</h2>
-          {user && (
-            <div>
-              <p>{user.name} logged in</p> <button onClick={() => handleLogOut()}>logout</button>
-            </div>
-          )}
-          <h2>create new</h2>
-              <form onSubmit={addBlog}>
-                title: <input type="text" value={newTitle} onChange={(event) => setNewTitle(event.target.value)} />
-                <br/>
-                author: <input type="text" value={newAuthor} onChange={(event) => setNewAuthor(event.target.value)} />
-                <br/>
-                url: <input type="text" value={newUrl} onChange={(event) => setNewUrl(event.target.value)} />
-                <br/>
-                <button type="submit">create</button>
-              </form>
-              
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />)}
-        </div>
-      )}
-      
+        {!user && loginForm()}
+        {user && (
+          <div>
+            <h2>blogs</h2>
+            <Notification message={message}/>
+            {user && (
+              <div>
+                <p style={{display: 'inline-flex'}}>{user.name} logged in</p>
+                <button style={{display: 'inline-flex'}} onClick={() => handleLogOut()}>logout</button>
+              </div>
+            )}
+            <h2>create new</h2>
+            <form onSubmit={addBlog}>
+              title: <input type="text" value={newTitle} onChange={(event) => setNewTitle(event.target.value)} />
+              <br/>
+              author: <input type="text" value={newAuthor} onChange={(event) => setNewAuthor(event.target.value)} />
+              <br/>
+              url: <input type="text" value={newUrl} onChange={(event) => setNewUrl(event.target.value)} />
+              <br/>
+              <button type="submit">create</button>
+            </form>          
+            {blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />)}
+          </div>
+        )}
     </div>
   )
 }
+
 
 export default App
